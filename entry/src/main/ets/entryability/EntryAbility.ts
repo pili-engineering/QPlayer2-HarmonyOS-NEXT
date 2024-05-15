979d63282b365b7029a43d271a796c9ef27f6add
Mengwangchao <www.903064748@qq.com>
2024-05-15 17:05:03 +0800

    申请后台长时任务 注册avsession 新增后台播放权限

--- entry/src/main/ets/entryability/EntryAbility.ts
+++ entry/src/main/ets/entryability/EntryAbility.ts
@@ -1,12 +1,24 @@
 import UIAbility from '@ohos.app.ability.UIAbility';
 import hilog from '@ohos.hilog';
 import window from '@ohos.window';
+import { WantAgent, wantAgent } from '@kit.AbilityKit';
+import { backgroundTaskManager } from '@kit.BackgroundTasksKit';
+import { BusinessError } from '@kit.BasicServicesKit';
+import AVSessionManager from '@ohos.multimedia.avsession';
 
+function callback(err: BusinessError, data: void) {
+  if (err) {
+    console.error("Operation startBackgroundRunning failed Cause: " + err);
+  } else {
+    console.info("Operation startBackgroundRunning succeeded");
+  }
+}
 export default class EntryAbility extends UIAbility {
 
   onCreate(want, launchParam) {
     AppStorage.setOrCreate("context",this.context)
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onCreate');
+
   }
 
   onDestroy() {
@@ -36,9 +48,40 @@ export default class EntryAbility extends UIAbility {
     // Ability has brought to foreground
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onForeground');
   }
+  async createSession() {
+    let type: AVSessionManager.AVSessionType = 'audio';
+    let session = await AVSessionManager.createAVSession(this.context,'SESSION_NAME', type);
 
+    // 激活接口要在元数据、控制命令注册完成之后再执行
+    await session.activate();
+    console.info(`session create done : sessionId : ${session.sessionId}`);
+  }
   onBackground() {
     // Ability has back to background
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onBackground');
+    this.createSession()
+    let wantAgentInfo : wantAgent.WantAgentInfo = {
+      wants: [
+        {
+          bundleName: "com.qiniu.qplayer2demo",
+          abilityName: "EntryAbility"
+        }
+      ],
+      // 点击通知后，动作类型
+      operationType: wantAgent.OperationType.START_ABILITY,
+      requestCode: 0,
+      // 点击通知后，动作执行属性
+      wantAgentFlags: [wantAgent.WantAgentFlags.UPDATE_PRESENT_FLAG]
+    };
+
+    wantAgent.getWantAgent(wantAgentInfo).then((wantAgentObj : WantAgent) => {
+      hilog.info(0x0000, 'testTag', '%{public}s', 'getWantAgent');
+      backgroundTaskManager.startBackgroundRunning(this.context,
+        backgroundTaskManager.BackgroundMode.AUDIO_PLAYBACK, wantAgentObj).then(() => {
+        console.info("Operation startBackgroundRunning succeeded");
+      }).catch((err: BusinessError) => {
+        console.error("Operation startBackgroundRunning failed Cause: " + err);
+      });
+    });
   }
 }
