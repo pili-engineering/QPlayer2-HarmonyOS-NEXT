48e4cec1573fce02cc1dbf71344da6aa801cbfdf
Mengwangchao <www.903064748@qq.com>
2024-08-05 10:44:25 +0800

    新增脚本，生成构建时间和设置依赖方式

--- entry/hvigorfile.ts
+++ entry/hvigorfile.ts
@@ -1,6 +1,27 @@
 import { hapTasks } from '@ohos/hvigor-ohos-plugin';
+import { OhosPluginId } from '@ohos/hvigor-ohos-plugin';
+// 自定义插件代码
+export function setPackage(dependenciesName : string, versionName : string): HvigorPlugin {
+    return {
+        pluginId: 'customPlugin',
+        async apply(currentNode: HvigorNode): Promise<void> {
+            const rootNodeContext = currentNode.getContext(OhosPluginId.OHOS_HAP_PLUGIN);
+            if (!rootNodeContext) {
+                return;
+            }
+            const dependenciesInfo = rootNodeContext.getDependenciesOpt()
+            dependenciesInfo["@qiniu/qplayer2-core"] = versionName
+            console.log(`setPackage : ` + dependenciesName + ` version : ` + dependenciesInfo["@qiniu/qplayer2-core"]);
+            rootNodeContext.setDependenciesOpt(dependenciesInfo);
+        }
+    };
+}
 
 export default {
     system: hapTasks,  /* Built-in plugin of Hvigor. It cannot be modified. */
-    plugins:[]         /* Custom plugin to extend the functionality of Hvigor. */
+    plugins:[
+        // setPackage("ohpm","^1.5.0-preview4")
+      setPackage("source","file:../qplayer2_core")
+      // setPackage("local","file:./dependency/qplayer2_core.har")
+    ]         /* Custom plugin to extend the functionality of Hvigor. */
 }
--- hvigorfile.ts
+++ hvigorfile.ts
@@ -1,4 +1,19 @@
-import { appTasks } from '@ohos/hvigor-ohos-plugin';
+
+import { appTasks, OhosPluginId, OhosAppContext, AppJson } from '@ohos/hvigor-ohos-plugin';
+import { hvigor, getNode, HvigorNode  } from '@ohos/hvigor';
+
+hvigor.nodesEvaluated(() => {
+    const node: HvigorNode = getNode(__filename);
+    const appContext = node.getContext(OhosPluginId.OHOS_APP_PLUGIN) as OhosAppContext;
+    const appJson5: AppJson.AppOptObj = appContext.getAppJsonOpt();
+
+    let date = new Date();
+    let currentTime = date.toLocaleString('zh-cn');
+
+    appJson5.app.vendor = currentTime;
+
+    appContext.setAppJsonOpt(appJson5);
+});
 
 export default {
     system: appTasks,  /* Built-in plugin of Hvigor. It cannot be modified. */
